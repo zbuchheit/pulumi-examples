7af9c6b8b07bc53f21ebc08ed74b97007a624a5b
zbuchheit <zbuchheit@pulumi.com>
2023-09-21 15:23:51 -0700

    add learn-stack-references based on pulumi.com/learn/

---
+++ learn-stack-references/index.ts
@@ -0,0 +1,9 @@
+import * as pulumi from "@pulumi/pulumi";
+
+const config = new pulumi.Config();
+const stack = pulumi.getStack();
+const org = config.require("org");
+
+const stackRef = new pulumi.StackReference(`${org}/learn-building/${stack}`)
+
+export const shopUrl = stackRef.getOutput("url");
