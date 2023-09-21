322ee1793a93b3ed65e522feecf9262d35bcbbd8
zbuchheit <zbuchheit@pulumi.com>
2023-09-21 13:52:20 -0700

    update eks ts example

--- aws-ts-eks/index.ts
+++ aws-ts-eks/index.ts
@@ -1,7 +1,8 @@
 import * as awsx from "@pulumi/awsx";
 import * as aws from "@pulumi/aws";
 import * as eks from "@pulumi/eks";
-import * as pulumi from "@pulumi/pulumi"
+import * as pulumi from "@pulumi/pulumi";
+import * as k8s from "@pulumi/kubernetes";
 
 const name = "zach-example";
 
@@ -10,10 +11,10 @@ const config = new pulumi.Config();
 const minClusterSize = config.getNumber("minClusterSize") || 3;
 const maxClusterSize = config.getNumber("maxClusterSize") || 6;
 const desiredClusterSize = config.getNumber("desiredClusterSize") || 3;
-const eksNodeInstanceType = config.get("eksNodeInstanceType") || aws.ec2.InstanceType.T3a_Nano;
+const eksNodeInstanceType = config.get("eksNodeInstanceType") || aws.ec2.InstanceType.T3_Medium;
 const vpcNetworkCidr = config.get("vpcNetworkCidr") || "10.0.0.0/16";
 
-// Define a new VPC with 3 subnets, 3 availability zones, and a single NAT gateway.
+// // Define a new VPC with 3 subnets, 3 availability zones, and a single NAT gateway.
 const eksVpc = new awsx.ec2.Vpc(`${name}-vpc`, {
     cidrBlock: vpcNetworkCidr,
     enableDnsHostnames: true,
@@ -22,7 +23,7 @@ const eksVpc = new awsx.ec2.Vpc(`${name}-vpc`, {
     }
 });
 
-const cluster = new eks.Cluster("myCluster", {
+const cluster = new eks.Cluster("zbuchheit-cluster", {
     vpcId: eksVpc.vpcId,
     publicSubnetIds: eksVpc.publicSubnetIds,
     privateSubnetIds: eksVpc.privateSubnetIds,
@@ -41,29 +42,48 @@ const cluster = new eks.Cluster("myCluster", {
     }
 });
 
-// const managed_node_group_spot = new eks.ManagedNodeGroup(`${name}-manangednodegroup-spot`, {
-//     cluster: cluster,
-//     capacityType: "SPOT",
-//     instanceTypes: [aws.ec2.InstanceType.T3a_Nano],
-//     nodeRole: cluster.instanceRoles[0],
-//     labels: {"managed":"true", "spot":"true"},
-//     tags: {
-//         "k8s.cluster-autoscaler/zbuchheit-dev":"owned",
-//         "k8s.cluster-autoscaler/enabled":"True",
-//         "team":"ce-team",
-//         "environment":"development"
-//     },
-//     scalingConfig: {
-//                     desiredSize: 2, 
-//                     minSize: 1, 
-//                     maxSize:10
-//                    },
-//     diskSize: 10,
-// })
+const k8sProvider = new k8s.Provider("k8s-provider", {
+    kubeconfig: cluster.kubeconfig
+})
+
+const appLabels = { app: "nginx" };
+
+const deployment = new k8s.apps.v1.Deployment(`k8s-deployment`, {
+    metadata: { 
+        name: "nginx-deployment",
+        labels: appLabels
+    },
+    spec: {
+        replicas: 1,
+        selector: { matchLabels: appLabels },
+        template: {
+            metadata: { labels: appLabels },
+            spec: {
+                containers: [{
+                    name: "nginx",
+                    image: "nginx",
+                    ports: [{ name: "http", containerPort: 80 }],
+                }],
+            },
+        },
+    },
+}, { provider: k8sProvider });
+
+export const labels = deployment.metadata.labels;
+
+const nginxService = new k8s.core.v1.Service("nginx-service", {
+    metadata: { name: "nginx-service" },
+    spec: {
+        selector: { app: "nginx" },
+        type: "LoadBalancer",
+        ports: [{ port: 80, targetPort: 80 }],
+    },
+}, { provider: k8sProvider });
+
+export const nginxLoadBalancerAddress = nginxService.status.loadBalancer.ingress[0].hostname;
 
 export const clusterConfig = pulumi.secret(cluster.kubeconfig);
 export const kubeVersion = cluster.eksCluster.version;
 export const vpcId = eksVpc.vpcId;
 export const vpcPublicSubnet = eksVpc.publicSubnetIds;
 export const vpcPrivateSubnet = eksVpc.privateSubnetIds;
-
